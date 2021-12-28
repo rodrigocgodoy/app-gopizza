4fdeea21d968680ec2c7a1d730d6dcf951204a83
Rodrigo Godoy <rodrigocgodoy@hotmail.com>
2021-12-28 03:07:44 -0300

    Adding statusBar global

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import AppLoading from 'expo-app-loading';
@@ -20,6 +21,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style='light' translucent backgroundColor='transparent' />
       <SignIn />
     </ThemeProvider>
   );
